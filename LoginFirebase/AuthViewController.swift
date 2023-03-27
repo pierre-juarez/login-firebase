38e05c0374f78878ac82f33186e1073528b685d5
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2023-03-27 00:33:56 -0500

    [add] - Initial commit 🚀

---
+++ LoginFirebase/AuthViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AuthViewController.swift
+//  LoginFirebase
+//
+//  Created by Pierre Juarez U. on 27/03/23.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
